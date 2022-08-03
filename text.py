36e8d1689139355252446ed3f25506e33143fa12
Toma400 <thomasso_calgori@o2.pl>
2022-08-03 13:31:48 +0200

    All stuff leftout

---
+++ text.py
@@ -0,0 +1,19 @@
+class TextHelpers:
+
+    init = f"""
+------------------------------------------------------------------------------
+| WELCOME TO SP>RIT                                                          |
+| Sp>rit is small interpreted language designed as learning tool and coding  |
+| challenge for programmers.                                                 |
+| Terminal accepts full path to the .sp/.sr files, as well as some reserved  |
+| keywords. Type 'help' to see additional keywords and their use.            |
+------------------------------------------------------------------------------
+"""
+    help = f"""
+------------------------------------------------------------------------------
+| SP>RIT HELP                                                                |
+| Language of Sp>rit uses some keywords in its terminal:                     |
+| -----------------------------------------------                            |
+| exit | closes the program                                                  |
+------------------------------------------------------------------------------
+"""
