8edfec91892dc34265d7aa79591bf6b1856e0c6a
Toma400 <thomasso_calgori@o2.pl>
2023-02-20 15:36:34 +0100

    Swapped Py base branch to Ruby

---
+++ root.rb
@@ -0,0 +1,54 @@
+#=============================================================================
+# SPIRIT
+# v.0.1.
+#=============================================================================
+# ANALYSIS:
+def file_analyser(file)
+  contents = file.readlines.map(&:chomp)
+  commands = []
+  puts "==============================="
+  for i in contents
+    puts i
+    commands << i.chomp
+  end
+  puts "Printing the command array:: [#{commands}]"
+  puts "==============================="
+
+end
+#=============================================================================
+# ERRORS:
+def wrong_path
+  puts "==============================="
+  puts "Analysis failed! Prompted path is not correct .sp file!"
+  exit!
+end
+#=============================================================================
+# READER:
+def prompt_reader(prompt)
+  puts "==============================="
+  puts "Analysing:: [#{prompt.chomp}]"
+  if prompt.chomp.include? ".sp"
+    begin
+      file = File.open(prompt.chomp)
+      file_analyser(file)
+      file.close
+    rescue
+      wrong_path
+    end
+  else
+    wrong_path
+  end
+end
+
+#=============================================================================
+# MAIN LOOP:
+while true
+  print ">> "
+  prompt = gets
+
+  if prompt.chomp == "end" # <--- not working
+    exit!
+  else
+    prompt_reader(prompt)
+  end
+end
